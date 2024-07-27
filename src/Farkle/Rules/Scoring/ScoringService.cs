8d3dc2a04ef5a188a01b3f01a789701039893199
Mike Bowers <mike@mikebowers.dev>
2024-07-26 20:30:32 -0400

    Corrected score values and adjust non-standard combinations

--- src/Farkle/Rules/Scoring/ScoringService.cs
+++ src/Farkle/Rules/Scoring/ScoringService.cs
@@ -15,32 +15,36 @@ public interface IScoringService
 /// Calculates a score given a set of selected dice.
 /// </summary>
 /// <remarks>
+/// <para>Standard Rules</para>
 /// <list type="table">
-/// <listheader>    
-///     <description>Pair Scoring</description>
-/// </listheader>
-///     <item> <term> Ones </term> <description> 100 </description> </item>
-///     <item> <term> Fives </term> <description> 50 </description> </item>
-///     <item> <term> 3 Ones </term> <description> 1000 </description> </item>
-///     <item> <term> 3 Twos </term> <description> 200 </description> </item>
-///     <item> <term> 3 Threes </term> <description> 300 </description> </item>
-///     <item> <term> 3 Fours </term> <description> 400 </description> </item>
-///     <item> <term> 3 Fives </term> <description> 500 </description> </item>
-///     <item> <term> 3 Sixes </term> <description> 600 </description> </item>
-///     <item> <term> 3 Pairs </term> <description> 1500 </description> </item>
-///     <item> <term> 2 Triplets </term> <description> 2500 </description> </item>
-///     <item> <term> 4 of a Kind </term> <description> 1000 </description> </item>
-///     <item> <term> 5 of a Kind </term> <description> 2000 </description> </item>
-///     <item> <term> 6 of a Kind </term> <description> 3000 </description> </item>
-///     <item> <term> 4 of a Kind and a Pair </term> <description> 1500 </description> </item>
-///     <item> <term> Straight (1-6) </term> <description> 1500 </description> </item>
-///     <item> <term> 3 Fails </term> <description> -1000 </description> </item>
+///     <item> <term> 1 </term> <description> 100 </description> </item>
+///     <item> <term> 5 </term> <description> 50 </description> </item>
+///     <item> <term> Three 1s </term> <description> 1000 </description> </item>
+///     <item> <term> Three 2s </term> <description> 200 </description> </item>
+///     <item> <term> Three 3s </term> <description> 300 </description> </item>
+///     <item> <term> Three 4s </term> <description> 400 </description> </item>
+///     <item> <term> Three 5s </term> <description> 500 </description> </item>
+///     <item> <term> Three 6s </term> <description> 600 </description> </item>
+///     <item> <term> Three Pairs </term> <description> 1500 </description> </item>
+///     <item> <term> Four of a Kind </term> <description> 1000 </description> </item>
+///     <item> <term> Five of a Kind </term> <description> 2000 </description> </item>
+///     <item> <term> Six of a Kind </term> <description> 3000 </description> </item>
+///     <item> <term> Straight (1-6) </term> <description> 2500 </description> </item>
+/// </list>
+/// 
+/// <para>Non-Standard Rules</para>
+/// <list type="table">
+///     <item> <term> Two Triplets </term> <description> 2500 </description> </item>
+///     <item> <term> Four of a Kind and a Pair </term> <description> 1500 </description> </item>
 /// </list>
 /// </remarks>
 public class ScoringService
 {
-    public ScoringService()
+    public bool AllowNonStandardCombinations { get; set; }
+
+    public ScoringService(bool allowNonStandardCombinations = false)
     {
+        AllowNonStandardCombinations = allowNonStandardCombinations;
     }
 
     public ScoredSet CalculateScore(IList<DiceSprite> diceSprites)
@@ -62,10 +66,12 @@ public class ScoringService
         {
             if (dice[0].Value == 1)
             {
+                // 1
                 score = 100;
             }
             else if (dice[0].Value == 5)
             {
+                // 5
                 score = 50;
             }
         }
@@ -73,52 +79,54 @@ public class ScoringService
         {
             if (dice[0].Value == 1)
             {
+                // Three 1s
                 score = 1000;
             }
             else
             {
+                // Three of a kind (except 1s)
                 score = dice[0].Value * 100;
             }
         }
         else if (dice.Count == 4 && distinctValues.Count == 1)
         {
-            // 4 of a kind
+            // Four of a kind
             score = 1000;
         }
         else if (dice.Count == 5 && distinctValues.Count == 1)
         {
-            // 5 of a kind
+            // Five of a kind
             score = 2000;
         }
         else if (dice.Count == 6 && distinctValues.Count == 1)
         {
-            // 6 of a kind
+            // Six of a kind
             score = 3000;
         }
-        else if (dice.Count == 6 && distinctValues.Count == 2)
+        else if (dice.Count == 6 && distinctValues.Count == 2 && AllowNonStandardCombinations)
         {
             var numberOfFirstValue = dice.Count(x => x.Value == distinctValues[0]);
-                                
+
             if (numberOfFirstValue == 3)
             {
-                // 2 triplets
+                // Two triplets
                 score = 2500;
             }
             else if (numberOfFirstValue == 2 || numberOfFirstValue == 4)
             {
-                // 4 of a kind and a pair
+                // Four of a kind and a pair
                 score = 1500;
             }
         }
         else if (dice.Count == 6 && distinctValues.Count == 3)
         {                   
-            // 3 pairs
-            score = 1500;                
+            // Three pairs
+            score = 1500;
         }
         else if (dice.Count == 6 && distinctValues.Count == 6)
         {
-            // straight 1-6
-            score = 1500;
+            // Straight 1-6
+            score = 2500;
         }
 
         var scoredSet = new ScoredSet(dice, score);

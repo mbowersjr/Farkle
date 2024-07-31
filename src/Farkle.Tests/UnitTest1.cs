04ec5539ed6d6bdc67ca2e23be1b7e2841e25dd0
gptaylor <gary.taylor717@gmail.com>
2024-07-31 00:15:21 -0400

    Initial commit

--- src/Farkle.Tests/UnitTest1.cs
+++ src/Farkle.Tests/UnitTest1.cs
@@ -5,6 +5,10 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        //unit test for 100% on one side
     }
+
+    //GameStateManager.ScoreSelectedDice
+
+    //ScoringService.CalculateScore(IList<DiceBase> dice)
 }
